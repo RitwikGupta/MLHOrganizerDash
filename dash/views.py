c59947c250227b21320ad0ac19aa326289370150
Ritwik Gupta <ritwikg2004@live.com>
2015-12-29 01:01:19 -0500

    Added sortify.js

--- dash/views.py
+++ dash/views.py
@@ -32,18 +32,6 @@ def stats():
 
     return render_template('stats.html', data=data, stats=stats)
 
-@dash.route('/sorted_univ', strict_slashes=False)
-def sort_univ():
-    return render_template('index.html', data=sorted(data, key=lambda x: x["school"]["name"]), hackathon_name=hackathon_name)
-
-@dash.route('/sorted_name', strict_slashes=False)
-def sort_name():
-    return render_template('index.html', data=sorted(data, key=lambda x: x["first_name"].lower() + " " + x["last_name"].lower()), hackathon_name=hackathon_name)
-
-@dash.route('/sorted_gender', strict_slashes=False)
-def sort_gender():
-    return render_template('index.html', data=sorted(data, key=lambda x: x["gender"]), hackathon_name=hackathon_name)
-
 if __name__ == '__main__':
     url = "https://my.mlh.io/api/v1/users?client_id={0}&secret={1}".format(client_id, secret)
     response = urllib.urlopen(url)
