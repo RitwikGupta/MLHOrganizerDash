01c12b1a702deec5e6e621ce8f433092aa85f3e9
Ritwik Gupta <ritwikg2004@live.com>
2015-12-28 23:06:45 -0500

    Added option to sort

--- dash/views.py
+++ dash/views.py
@@ -13,6 +13,18 @@ data = []
 def index():
     return render_template('index.html', data=data, hackathon_name=hackathon_name)
 
+@dash.route('/sorted_univ', strict_slashes=False)
+def sort_univ():
+    return render_template('index.html', data=sorted(data, key=lambda x: x["school"]["name"]), hackathon_name=hackathon_name)
+
+@dash.route('/sorted_name', strict_slashes=False)
+def sort_name():
+    return render_template('index.html', data=sorted(data, key=lambda x: x["first_name"] + " " + x["last_name"]), hackathon_name=hackathon_name)
+
+@dash.route('/sorted_gender', strict_slashes=False)
+def sort_gender():
+    return render_template('index.html', data=sorted(data, key=lambda x: x["gender"]), hackathon_name=hackathon_name)
+
 if __name__ == '__main__':
     url = "https://my.mlh.io/api/v1/users?client_id={0}&secret={1}".format(client_id, secret)
     response = urllib.urlopen(url)
