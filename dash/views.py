7667cc4ea5f40ea0a2cf8f184db3acebaa033246
Ritwik Gupta <ritwikg2004@live.com>
2015-12-28 22:19:04 -0500

    Initial registration view

---
+++ dash/views.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+from config import client_id, secret, hackathon_name
+
+import urllib, json
+
+dash = Flask(__name__)
+url = ""
+data = []
+
+
+@dash.route('/')
+def index():
+    print data
+    return render_template('index.html', data=data, hackathon_name=hackathon_name)
+
+if __name__ == '__main__':
+    url = "https://my.mlh.io/api/v1/users?client_id={0}&secret={1}".format(client_id, secret)    
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())["data"]
+
+    dash.run()
