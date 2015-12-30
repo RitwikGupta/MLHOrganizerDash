489271f996b5a1642ee604a477544cb8402cca8f
Ritwik Gupta <ritwikg2004@live.com>
2015-12-29 22:18:19 -0500

    Added median highlighting

--- dash/views.py
+++ dash/views.py
@@ -31,7 +31,21 @@ def stats():
     stats["university"] = sorted(stats["university"].iteritems(), key=lambda x: x[1], reverse=True)
     stats["university"] = map(lambda x: (x[0].lower().strip(), x[1]), stats["university"])
 
-    return render_template('stats.html', data=data, stats=stats)
+    med = median(list(set(map(lambda x: x[1], stats["university"])))) # Lol
+
+    print "MED: " + str(med)
+
+    return render_template('stats.html', data=data, stats=stats, median=med)
+
+
+def median(lst):
+    lst = sorted(lst)
+    if len(lst) < 1:
+            return None
+    if len(lst) %2 == 1:
+            return lst[((len(lst)+1)/2)-1]
+    else:
+            return float(sum(lst[(len(lst)/2)-1:(len(lst)/2)+1]))/2.0
 
 if __name__ == '__main__':
     url = "https://my.mlh.io/api/v1/users?client_id={0}&secret={1}".format(client_id, secret)
