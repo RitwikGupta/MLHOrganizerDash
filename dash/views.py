0098b41c8ccca32d1aa9078b769e06e7221aed6f
Ritwik Gupta <ritwikg2004@live.com>
2015-12-29 23:11:19 -0500

    Added refresh button

--- dash/views.py
+++ dash/views.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 from config import client_id, secret, hackathon_name
 
 import urllib, json
 
 dash = Flask(__name__)
+
 url = ""
 data = []
 
@@ -35,15 +36,22 @@ def stats():
 
     return render_template('stats.html', data=data, stats=stats, median=med)
 
+@dash.route('/refresh', strict_slashes=False)
+def refresh():
+    url = "https://my.mlh.io/api/v1/users?client_id={0}&secret={1}".format(client_id, secret)
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())["data"]
+
+    return redirect('/')
 
 def median(lst):
     lst = sorted(lst)
     if len(lst) < 1:
-            return None
+        return None
     if len(lst) %2 == 1:
-            return lst[((len(lst)+1)/2)-1]
+        return lst[((len(lst)+1)/2)-1]
     else:
-            return float(sum(lst[(len(lst)/2)-1:(len(lst)/2)+1]))/2.0
+        return float(sum(lst[(len(lst)/2)-1:(len(lst)/2)+1]))/2.0
 
 if __name__ == '__main__':
     url = "https://my.mlh.io/api/v1/users?client_id={0}&secret={1}".format(client_id, secret)
