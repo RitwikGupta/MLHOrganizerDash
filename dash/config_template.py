ded7a4e535a4f9074cb9bae0c4d395fdbc99170c
Kevin Dice <kevin.dice1@gmail.com>
2016-04-05 15:27:25 -0400

    Add examples to the config for typo mapping

--- dash/config_template.py
+++ dash/config_template.py
@@ -1,4 +1,4 @@
 client_id = "YOUR CLIENT ID"
 secret = "YOUR SECRET"
 hackathon_name = "YOUR HACKATHON NAME"
-typo_mapping = {}
+typo_mapping = {'kansas state university': 'Kansas State University', 'Uinversity of Kansas': 'University of Kansas'}
