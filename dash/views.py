1c68dd19998a5f2a82871ea22f6f80deeb21ca4f
Kevin Dice <kevin.dice1@gmail.com>
2016-04-05 15:12:00 -0400

    Remove trailing whitespace

--- dash/views.py
+++ dash/views.py
@@ -5,7 +5,7 @@ from config import client_id, secret, hackathon_name, typo_mapping
 import urllib, json
 import datetime
 import random
-import os 
+import os
 
 from collections import defaultdict
 
@@ -38,10 +38,10 @@ def stats():
 
     stats["diet_rest"] = sorted(stats["diet_rest"].iteritems(), key=lambda x: x[1], reverse=True)
     stats["diet_rest"] = map(lambda x: (x[0].lower().strip(), x[1]), stats["diet_rest"])
-   
+
     med = median(list(set(map(lambda x: x[1], stats["university"])))) # Lol
 
-    stats["university"] = dict(stats["university"])    
+    stats["university"] = dict(stats["university"])
     for typo in typo_mapping:
         stats["university"][typo_mapping[typo]] += stats["university"][typo]
         del stats["university"][typo]
@@ -70,14 +70,14 @@ def median(lst):
 #@dash.route('/getgraphs', strict_slashes=False)
 #def getgraphs():
 
-  
+
 #    canvas=FigureCanvas(f)
 #    png_output = StringIO.StringIO()
 #    canvas.print_png(png_output)
 #    response=make_response(png_output.getvalue())
 #    response.headers['Content-Type'] = 'image/png'
 #    return response
-    
+
 if __name__ == '__main__':
     url = "https://my.mlh.io/api/v1/users?client_id={0}&secret={1}".format(client_id, secret)
     response = urllib.urlopen(url)
