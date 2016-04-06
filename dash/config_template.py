98d1abbe356570ded2a99046a3f6becd711ba77c
Ritwik Gupta <RitwikGupta@users.noreply.github.com>
2016-04-06 00:16:14 -0400

    Add some more universities to typo list

--- dash/config_template.py
+++ dash/config_template.py
@@ -1,4 +1,9 @@
 client_id = "YOUR CLIENT ID"
 secret = "YOUR SECRET"
 hackathon_name = "YOUR HACKATHON NAME"
-typo_mapping = {'kansas state university': 'Kansas State University', 'Uinversity of Kansas': 'University of Kansas'}
+typo_mapping = {
+  'kansas state university': 'Kansas State University',
+  'Uinversity of Kansas': 'University of Kansas',
+  "univeristy of pittsburgh": "university of pittsburgh",
+  "mcmaster universiry": "mcmaster university"
+}
